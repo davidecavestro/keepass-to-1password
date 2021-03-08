40a63caf5c130f5f764cffcf1484f192fd3fbafc
Davide Cavestro <davide.cavestro@gmail.com>
2021-03-08 18:13:34 +0100

    Update k1p.py
    Seems to work with python 3.8.6 and XML generated from Keepass 2.41 on Linux

--- k1p.py
+++ k1p.py
@@ -3,7 +3,7 @@
 import os
 import logging
 
-import ConfigParser
+import configparser
 import argparse
 from jinja2 import Environment, PackageLoader
 from bs4 import BeautifulSoup
@@ -25,33 +25,42 @@ parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
 args = parser.parse_args()
 
 # Read config file
-config = ConfigParser.SafeConfigParser()
+config = configparser.SafeConfigParser()
 config.read(os.path.join('etc', script_name + '.conf'))
 
-passwords_xml = BeautifulSoup(open(config.get('General', 'input')))
+passwords_xml = BeautifulSoup(open(config.get('General', 'input')), features="lxml")
 logger.info('KeePass XML file is opened')
 
 passwords = []
 
 for entry in passwords_xml.find_all('entry'):
   password = {}
-  password['title'] = normalize(entry.title.string)
-  if entry.username.string:
-    password['username'] = normalize(entry.username.string)
-  if entry.password.string:
-    password['password'] = normalize(entry.password.string)
-  if entry.url.string:
-    password['url'] = normalize(entry.url.string.replace('http://', ''))
-  if entry.comment.contents:
-    # Convert <br> to line breaks:
-    notes = '\n'.join(unicode(element) for element in entry.comment.contents if element.name != 'br')
-    password['notes'] = normalize(notes)
+  if entry.parent.name == 'history':
+    continue
+
+  # for el in entry.find_all('string'):
+  for el in entry.findChildren("string", recursive=False):
+    if not el.value.string:
+      continue
+
+    if el.key.string == 'Title':
+      password['title'] = normalize(el.value.string)
+    elif el.key.string == 'UserName':
+      password['username'] = normalize(el.value.string)
+    elif el.key.string == 'Password':
+      password['password'] = normalize(el.value.string)
+    elif el.key.string == 'URL':
+      password['url'] = normalize(el.value.string.replace('http://', ''))
+    elif el.key.string == 'Notes':
+      # notes = '\n'.join(unicode(element) for element in entry.comment.contents if element.name != 'br')
+      password['notes'] = normalize(el.value.string)
+
   passwords.append(password)
 
 # Prepare output file
 env = Environment(loader=PackageLoader('__main__', 'templates'))
 template = env.get_template('passwords.tmpl')
-output = open(config.get('General', 'output'), 'w')
+output = open(config.get('General', 'output'), 'wb')
 output.write(template.render(passwords = passwords).encode('utf-8'))
 output.close()
 
